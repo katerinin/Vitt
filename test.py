e3e8af28dc13e1813f2a3c775c7ec3b753d8cb8d
Viktor Katerinin <Katerininvv@yandex.ru>
2020-10-25 13:09:47 +0300

    ferst

---
+++ test.py
@@ -0,0 +1,15 @@
+
+def f (a, b):
+    if a==b:
+        print('gsd is', a)
+        return
+    elif a>b:
+        n=a-b
+        f (n,b)
+    elif a<b:
+        n=b-a
+        f (a,n)
+
+a =7 #int(input())
+b =3 #int(input())
+f(a,b)
